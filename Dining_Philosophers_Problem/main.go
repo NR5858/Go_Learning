486b057a67a53c5c3365c06838735391541ebaa5
Navin Ramkishun <navinr111@gmail.com>
2022-12-23 12:57:14 -0500

    Adding new Dining Philosophers problem

---
+++ Dining_Philosophers_Problem/main.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+The Dining Philosophers problem is well known in computer science circles.
+Five philosophers, numbers from 0 through 4, live in a house where the
+table is laid for them; each philosopher has their own place at the table.
+Their only difficulty - besides those of philosophy - is that the dish
+served is a very difficult kind of spaghetti which has to be eaten with
+two forks. There are two forks next to each plate, so that presents no
+difficulty. As a consequence, however, this means that no two neighbors
+may be eating simultaneously, since there are five philosophers and five forks.
+This is a simple implementation of Dijkstra's solution to the "Dining
+Philosophers" dilemma.
+*/
+
+// Philospher is a struct which stores information about a philosopher
+type Philosopher struct {
+	name      string
+	rightFork int
+	leftFork  int
+}
+
+func main() {
+
+}
