5b92a975d5aafd4a88376048851e736aff8a2150
Navin Ramkishun <navinr111@gmail.com>
2022-12-27 06:35:51 -0500

    Go Learning: Added go.work file for multiple module handling + Dining Philosophers basic logic completed

--- Dining_Philosophers_Problem/main.go
+++ Dining_Philosophers_Problem/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 /*
 The Dining Philosophers problem is well known in computer science circles.
 Five philosophers, numbers from 0 through 4, live in a house where the
@@ -12,14 +18,61 @@ may be eating simultaneously, since there are five philosophers and five forks.
 This is a simple implementation of Dijkstra's solution to the "Dining
 Philosophers" dilemma.
 */
-
-// Philospher is a struct which stores information about a philosopher
 type Philosopher struct {
 	name      string
 	rightFork int
 	leftFork  int
 }
 
-func main() {
+var philosophers = []Philosopher{
+	{name: "Plato", leftFork: 4, rightFork: 0},
+	{name: "Socrates", leftFork: 0, rightFork: 1},
+	{name: "Aristotle", leftFork: 1, rightFork: 2},
+	{name: "Pascal", leftFork: 2, rightFork: 3},
+	{name: "Locke", leftFork: 3, rightFork: 4},
+}
 
+var hunger = 3
+var eatTime = 1 * time.Second
+var thinkTime = 3 * time.Second
+var sleepTime = 1 * time.Second
+
+func main() {
+	// print out a welcome message
+	fmt.Println("Dining Philosophers Problem")
+	fmt.Println("---------------------------")
+	fmt.Println("The table is empty.")
+
+	// start the meal
+	dine()
+	// print out finished message
+	fmt.Println("The table is empty.")
+
+}
+
+func dine() {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(philosophers))
+
+	seated := &sync.WaitGroup{}
+	seated.Add(len(philosophers))
+
+	// forks is a map of all 5 forks.
+	var forks = make(map[int]*sync.Mutex)
+
+	for i := 0; i < len(philosophers); i++ {
+		forks[i] = &sync.Mutex{}
+	}
+
+	// start the meal.
+	for i := 0; i < len(philosophers); i++ {
+		// fire off a goroutine for the current philosopher
+		go diningProblem(philosophers[i], wg, forks, seated)
+	}
+
+	wg.Wait()
+}
+
+func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seated *sync.WaitGroup) {
+	defer wg.Done()
 }
