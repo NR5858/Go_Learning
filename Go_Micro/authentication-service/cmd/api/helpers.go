bd172e2bc2c168b0b3defb12356c28a025035bb3
Navin Ramkishun <navinr111@gmail.com>
2023-01-20 16:21:37 -0500

    Go Learning: Adding Authentication Service

--- Go_Micro/authentication-service/cmd/api/helpers.go
+++ Go_Micro/authentication-service/cmd/api/helpers.go
@@ -10,7 +10,7 @@ import (
 type jsonResponse struct {
 	Error   bool   `json:"error"`
 	Message string `json:"message"`
-	Data    any    `json:"data,omitempty`
+	Data    any    `json:"data,omitempty"`
 }
 
 func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
@@ -31,6 +31,8 @@ func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) er
 
 	return nil
 }
+
+// writeJSON takes a response status code and arbitrary data and writes a json response to the client
 
 func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
 	out, err := json.Marshal(data)
@@ -57,6 +59,8 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 	return nil
 }
 
+// errorJSON takes an error, and optionally a response status code, and generates and sends
+// a json error response
 func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
 	statusCode := http.StatusBadRequest
 
